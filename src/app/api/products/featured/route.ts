b99c8e9bb59ae1b9868e7b4c5379a7f90652ccf1
imperiano <imperiano.cn@gmail.com>
2024-04-25 11:38:14 -0300

    ci: create cypress e2e workflow

--- src/app/api/products/featured/route.ts
+++
@@ -1,7 +0,0 @@
-import data from '../data.json'
-
-export async function GET() {
-  await new Promise(resolve => setTimeout(resolve, 1000))
-  const featuredproducts = data.products.filter(produt => produt.featured)
-  return Response.json(featuredproducts)
-}
