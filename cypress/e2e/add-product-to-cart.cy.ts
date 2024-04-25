08bfd79bab9c48ac323bb57f0585bea6ba2e1fb5
imperiano <imperiano.cn@gmail.com>
2024-04-25 10:21:59 -0300

    add search page tests

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -1,39 +1,24 @@
-import { contains } from "cypress/types/jquery"
+import { contains } from 'cypress/types/jquery'
 
 describe('add product to cart', () => {
-
   beforeEach(() => {
     cy.visit('/')
   })
 
-    it('shold be able to navigate to the product page and add it to the cart', () => {
-     
+  it('shold be able to navigate to the product page and add it to the cart', () => {
+    cy.get('a[href^="/product"]').first().click()
 
-      cy.get('a[href^="/product"]').first().click()
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Cart (1)').should('exist')
+  })
 
-      cy.location('pathname').should('include', '/product')
-      cy.contains('Adicionar ao carrinho').click()
-      cy.contains('Cart (1)').should('exist')
-    })
+  it('shold not count duplicated products on cart', () => {
+    cy.get('a[href^="/product"]').first().click()
 
-    it('shold not count duplicated products on cart', () => {
-
-      cy.get('a[href^="/product"]').first().click()
-
-      cy.location('pathname').should('include', '/product')
-      cy.contains('Adicionar ao carrinho').click()
-      cy.contains('Adicionar ao carrinho').click()
-      cy.contains('Cart (1)').should('exist')
-    })
-
-    it('shold be able to search for a product and add it to the cart', () => {
-
-      cy.get('input[name=q]').type('moletom').parent('form').submit()
-      cy.location('pathname').should('include', '/search')
-
-      cy.get('a[href^="/product"]').first().click()
-      cy.location('pathname').should('include', '/product')
-      cy.contains('Adicionar ao carrinho').click()
-      cy.contains('Cart (1)').should('exist')
-    })
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Cart (1)').should('exist')
+  })
 })
