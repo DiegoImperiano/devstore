92eb4bc19739d2119fbe9b795ee0b49d558d0ab6
imperiano <imperiano.cn@gmail.com>
2024-04-25 10:09:46 -0300

    add cypress and tests E2E of add to cart

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,39 @@
+import { contains } from "cypress/types/jquery"
+
+describe('add product to cart', () => {
+
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+    it('shold be able to navigate to the product page and add it to the cart', () => {
+     
+
+      cy.get('a[href^="/product"]').first().click()
+
+      cy.location('pathname').should('include', '/product')
+      cy.contains('Adicionar ao carrinho').click()
+      cy.contains('Cart (1)').should('exist')
+    })
+
+    it('shold not count duplicated products on cart', () => {
+
+      cy.get('a[href^="/product"]').first().click()
+
+      cy.location('pathname').should('include', '/product')
+      cy.contains('Adicionar ao carrinho').click()
+      cy.contains('Adicionar ao carrinho').click()
+      cy.contains('Cart (1)').should('exist')
+    })
+
+    it('shold be able to search for a product and add it to the cart', () => {
+
+      cy.get('input[name=q]').type('moletom').parent('form').submit()
+      cy.location('pathname').should('include', '/search')
+
+      cy.get('a[href^="/product"]').first().click()
+      cy.location('pathname').should('include', '/product')
+      cy.contains('Adicionar ao carrinho').click()
+      cy.contains('Cart (1)').should('exist')
+    })
+})
